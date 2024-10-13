4c3d509554efabc3f8e4e60b7ef67e1721cbfbd7
AhmedRabea0302 <arabea169@gmail.com>
2024-10-13 18:08:14 +0300

    Added endpoints handlers

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/AhmedRabea0302/go_stripe_payment
 
 go 1.22.2
+
+require github.com/stripe/stripe-go/v80 v80.2.0 // indirect
--- main.go
+++ main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/stripe/stripe-go/paymentintent"
+	"github.com/stripe/stripe-go/v80"
 )
 
 func main() {
-	http.HandleFunc("/health", checkHealth)
+
+	stripe.Key = "YOUR_SECRET_KEY"
+
+	http.HandleFunc("/health", handleCheckHealth)
 	http.HandleFunc("/create-payment-intent", handleCreatePaymentIntent)
 
-	err := http.ListenAndServe(":8000", nil)
+	err := http.ListenAndServe(":8080", nil)
 
 	if err != nil {
 		log.Fatal(err)
@@ -20,9 +28,74 @@ func main() {
 }
 
 func handleCreatePaymentIntent(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
 
+	var req struct {
+		ProdcutId string `json:"prodcut_id"`
+		FirstName string `json:"first_name"`
+		LastName  string `json:"last_name"`
+		Address1  string `json:"addres_1"`
+		Address2  string `json:"address_2"`
+		City      string `json:"city"`
+		State     string `json:"state"`
+		Zip       string `json:"zip"`
+		Country   string `json:"country"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	params := &stripe.PaymentIntentParams{
+		Amount:   stripe.Int64(calculateOrderAmount(req.ProdcutId)),
+		Currency: stripe.String(string(stripe.CurrencyUSD)),
+		AutomaticPaymentMethods: &stripe.PaymentIntentAutomaticPaymentMethodsParams{
+			Enabled: stripe.Bool(true),
+		},
+	}
+
+	paymenIntnet, err := paymentintent.New(params)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	var response struct {
+		ClientSecret string `json:"clientSecret"`
+	}
+
+	paymenIntnet.ClientSecret = params.ClientSecret
+
+	var buf bytes.Buffer
+	err = json.NewDecoder(&buf).Encode(response)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
 }
 
-func checkHealth(w http.ResponseWriter, r *http.Request) {
+func handleCheckHealth(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode("Server is up and running")
+}
 
+func calculateOrderAmount(productId string) int64 {
+	switch productId {
+	case "Forever Pants":
+		return 26000
+	case "Forever Shirt":
+		return 15000
+	case "Forever Shorts":
+		return 18000
+	}
+
+	return 0
 }
